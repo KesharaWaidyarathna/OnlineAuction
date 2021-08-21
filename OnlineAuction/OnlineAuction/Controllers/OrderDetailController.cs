5d609094a6da1bfcb0c13bd74bec8783f1edcfc7
KeaharaWaidyarathna <waidyrathna@gmail.com>
2021-08-21 13:36:29 +0530

    Enable Corse

--- OnlineAuction/OnlineAuction/Controllers/OrderDetailController.cs
+++ OnlineAuction/OnlineAuction/Controllers/OrderDetailController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using OnlineAuction.Data;
 using OnlineAuction.DTO;
 
 namespace OnlineAuction.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class OrderDetailController : ApiController
     {
         OrderData OrderData = new OrderData();
